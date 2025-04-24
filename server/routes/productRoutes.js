8aa5e8ebecc33a92dd59e44e7644bab53fde98ef
TheHoneyBoy <paulauccacusihuanca@gmail.com>
2025-04-24 03:14:31 -0500

    Agrega API's para el manejo de Productos

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getAllProducts);
+router.post('/', productController.createProduct);
+router.delete('/:id', productController.deleteProduct);
+router.get('/:id', productController.getProductById);
+router.put('/:id', productController.updateProduct);
+
+module.exports = router;
