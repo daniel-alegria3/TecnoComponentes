e2ea7653063a9f0629dc8087c91c1bf0b239d6cf
TheHoneyBoy <paulauccacusihuanca@gmail.com>
2025-05-08 16:01:05 -0500

    añadiendo register y login en backend con procedimientos almacenados

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 //Cliente
 const clientController = require('../controllers/clientController.js')
 router.get('/', clientController.getAllProducts_client);
-//Para cliente usar 
-//
-//fetch('http://localhost:5000/api/products/client');
+// necesita: mail, password_encrypted
+router.post('/register', clientController.register);
+// necesita: mail, password_encrypted
+router.post('/login', clientController.login);
 module.exports = router;
