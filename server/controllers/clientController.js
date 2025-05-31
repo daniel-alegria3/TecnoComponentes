0d8a6fdd29fd57b696d42a2e9a88d544ad6a195b
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2025-05-31 03:15:41 -0500

    Organiza el backend para las sessiones del cliente

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -2,8 +2,9 @@ const pool = require('../database/db');
 
 const clientController = {
 
-  // Obtener todos los productos
+  // Obtener todos los productos del cliente
   getAllProducts_client: async (req, res) => {
+    // TODO: reimplementar
     try {
       const [rows] = await pool.query('SELECT * FROM Product');
       res.json(rows);
@@ -11,76 +12,6 @@ const clientController = {
       console.error('Error en <getAllProducts>:', error);
       res.status(500).json({ error: 'Error interno del servidor' });
     }
-  },
-
-  register: async (req, res) => {
-    const { mail, password } = req.body;
-
-    if (!mail || mail.trim() === '' || !password || password.trim() === '') {
-      return res.status(400).json({ error: 'Datos inválidos' });
-    }
-
-    try {
-      const [result] = await pool.query(
-        `CALL agregar_cliente(?, ?)`,
-        [mail.trim(), password]
-      );
-      const [rows] = await pool.query('SELECT * FROM Client WHERE mail = ?', [mail.trim()]);
-      if (rows.length === 0) return res.status(404).json({ error: 'Cliente no encontrado' });
-      req.session.id_client = rows[0].id_client; // req.session provided by express-session
-      res.status(201).json({ message: 'Cliente registrado correctamente'});
-    } catch (error) {
-      if (error?.sqlState === '45000') {
-        console.error('Error en <cliente.register>:', error);
-        res.status(500).json({ error: 'No se pudo crear el cliente: ' + error.sqlMessage});
-      }
-      else {
-        console.error('Error en <cliente.register>:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-      }
-    }
-  },
-
-  login: async (req, res) => {
-    const { mail, password } = req.body;
-
-    if (!mail || mail.trim() === '' || !password || password.trim() === '') {
-      return res.status(400).json({ error: 'Datos inválidos' });
-    }
-
-    try {
-      await pool.query(
-        `CALL login_cliente(?, ?)`,
-        [mail.trim(), password]
-      );
-      const [rows] = await pool.query('SELECT * FROM Client WHERE mail = ?', [mail.trim()]);
-      req.session.id_client = rows[0].id_client; // req.session provided by express-session
-      res.status(201).json({ message: 'Login exitoso' });
-    } catch (error) {
-      if (error?.sqlState === '45000') {
-        console.error('Error en <cliente.login>:', error);
-        res.status(500).json({ error: 'Error en login: ' + error.sqlMessage});
-      }
-      else {
-        console.error('Error en <cliente.login>:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-      }
-    }
-  },
-
-  logged_in: async (req, res) => {
-    if (req.session.id_client) {
-      res.json({ loggedIn: true, id_client: req.session.id_client});
-    } else {
-      res.json({ loggedIn: false });
-    }
-  },
-
-  logout: async (req, res) => {
-    req.session.destroy(() => {
-      res.clearCookie('my-client-cookie-name.sid');
-      res.json({ success: true });
-    });
   },
 };
 
