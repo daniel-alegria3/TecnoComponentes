e71a15b8504dede434994c06684e77fcefc613d4
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2025-05-31 03:15:41 -0500

    Organiza el backend para las sessiones del cliente

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -2,35 +2,17 @@ const express = require('express');
 const router = express.Router();
 const session = require('express-session');
 
-/// Permite que router use express-session
-router.use(session({
-  name: 'my-client-cookie-name.sid',
-  secret: 'my-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false, // set to true in production with HTTPS
-    httpOnly: true,
-    sameSite: 'lax',
-    maxAge: 1000 * 60 * 60 // 1 hour
-  }
-}));
-
-/// Middleware para verificar session de usuario
-function requireAuth(req, res, next) {
-  if (!req.session.id_client)
-    return res.status(401).json({ error: 'Cliente no ha iniciado sesion.' });
-  next();
-};
-
 //Cliente
-const clientController = require('../controllers/clientController.js')
-router.post('/register', clientController.register);
-router.post('/login', clientController.login);
-router.get('/logged_in', clientController.logged_in);
-router.post('/logout', clientController.logout);
+const cauth = require('../middlewares/clientAuth');
+cauth.init(router)
 
-router.get('/', requireAuth, clientController.getAllProducts_client);
+router.post('/register', cauth.register);
+router.post('/login', cauth.login);
+router.get('/logged_in', cauth.isLoggedIn);
+router.post('/logout', cauth.requireLogin, cauth.logout);
+
+const clientController = require('../controllers/clientController.js')
+router.get('/', cauth.requireLogin, clientController.getAllProducts_client);
 
 module.exports = router;
 
